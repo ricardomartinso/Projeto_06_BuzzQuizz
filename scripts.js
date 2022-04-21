c7ac5eff839134d881fbd2ce824eb72166981c21
Ricardo <ricardomartinso.dev@gmail.com>
2022-04-20 21:09:09 -0300

    teste merge

--- scripts.js
+++ scripts.js
@@ -75,12 +75,13 @@ function selecionarQuizz(elemento){
 
     console.log(questoesQuizz[0].answers);
     listaPerguntasQuizz = questoesQuizz;
-
+    console.log("oi");
 }
 
 function verificarCorreta(elemento){
     const conjuntoRespostas = elemento.parentNode;
     conjuntoRespostas.querySelectorAll(".resposta")
+    console.log("oi");
 }
 
 
